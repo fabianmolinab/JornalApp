8a49a1494922d89bc5142695ae7ca63f711a53d4
fabianmolinab <molinafabian11b@gmail.com>
2022-02-05 22:35:43 -0500

    Configuraciones iniciales Terminadas

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export const JornalApp = (props: {}) => {
-  return (
-    <div>
-      <h1>Jornal App</h1>
-    </div>
-  )
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
 import { JornalApp } from './JornalApp'
+import './styles/styles.scss'
 
 ReactDOM.render(<JornalApp />, document.getElementById('root'))
