98d3d52bdcf6f50be2eb743159534fa6e03d44ed
fabianmolinab <molinafabian11b@gmail.com>
2022-03-06 23:54:18 -0500

    Diseño de HomeScreen [Interface] :D

--- src/components/notes/NoteScreen.tsx
+++ src/components/notes/NoteScreen.tsx
@@ -4,6 +4,26 @@ export const NoteScreen = (props: {}) => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+
+        <textarea
+          placeholder="What happened today"
+          className="notes__textarea"
+        ></textarea>
+
+        <div className="notes__images">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/09/07/21/34/child-438373_960_720.jpg"
+            alt="notes images"
+          />
+        </div>
+      </div>
     </div>
   )
 }
