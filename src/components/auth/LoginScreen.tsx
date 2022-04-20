00520d0f017387f9f1c79f685aa54a3342fbfc88
fabianmolinab <molinafabian11b@gmail.com>
2022-04-20 03:27:24 -0500

    UseForm Implementado

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -1,23 +1,48 @@
+import { ChangeEvent } from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 export const LoginScreen = (props: {}) => {
+  const { email, password, handleInputChange, reset } = useForm<FormData>({
+    email: '',
+    password: ''
+  })
+
+  type HandleSubmitEvent = ChangeEvent<HTMLFormElement>;
+
+  const handleInputSubmit = (e: HandleSubmitEvent) => {
+    e.preventDefault()
+    reset()
+  }
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form>
+      <form onSubmit={handleInputSubmit}>
         <input
           className="auth__input"
-          type="text"
+          type="email"
           placeholder="Email"
           name="email"
           autoComplete="off"
+          onChange={handleInputChange}
+          value={email}
         />
+
         <input
           className="auth__input"
           type="password"
           placeholder="Password"
           name="password"
+          onChange={handleInputChange}
+          value={password}
         />
+
         <button className="btn btn-primary mb-1" type="submit">
           Login
         </button>
@@ -37,6 +62,7 @@ export const LoginScreen = (props: {}) => {
             <p className="btn-text">
               <b>Sign in with google</b>
             </p>
+
           </div>
         </div>
         <Link className="links" to="/auth/register">
