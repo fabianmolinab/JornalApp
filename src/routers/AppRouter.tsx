128dc3e776555c83c531fd28eed386716bf2e863
Fabian Molina <molinafabian11b@gmail.com>
2022-06-22 03:06:16 -0500

    Muestra el loading global que autentica el usuario

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import { login } from '../actions/auth'
@@ -10,18 +10,30 @@ export const AppRouter: React.FC = () => {
 
   const dispatch = useDispatch()
 
+  const [checking, setChecking] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  // useEffect solo se dispara al iniciar la aplicación se encarga de mantener el estado de la autenticación
   useEffect(() => {
 
     const auth = getAuth()
-
     onAuthStateChanged(auth, (user) => {
 
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName))
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
       }
 
+      setChecking(false)
     })
-  }, [])
+  }, [dispatch, setChecking])
+
+  if (checking) {
+    return <h1>Espere...</h1>
+  }
+
   return (
     <Switch>
       <Route path="/auth" component={AuthRouter} />
